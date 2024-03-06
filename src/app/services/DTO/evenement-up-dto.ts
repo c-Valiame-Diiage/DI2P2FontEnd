d4013ced40eecbea65da40e660d08ad0e66c08aa
Cécile VALIAMÉ <cecile.valiame@diiage.org>
2024-03-06 15:28:13 +0100

    Setup environment variables and API services

---
+++ src/app/services/DTO/evenement-up-dto.ts
@@ -0,0 +1,7 @@
+export interface EvenementUpDTO {
+	titre: string;
+    description: string;
+    dateEvent: string | null;
+    timeEvent: string | null;
+    lieu: string;
+}
---
+++ src/app/services/evenement/evenement.service.ts
@@ -0,0 +1,71 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError } from 'rxjs';
+import { environment } from '../../../environments/environment.development';
+import { Evenement } from '../../models/evenement';
+import { EvenementUpDTO } from '../DTO/evenement-up-dto';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EvenementService {
+  private evenementsUrl = 'Evenements'; // URL to AzureFunction
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
+  constructor(private http: HttpClient) {}
+
+  /** GET Evenements from the server*/
+  getEvenements(): Observable<Evenement[]> {
+    return this.http.get<Evenement[]>(
+      `${environment.apiUrl}${this.evenementsUrl}`
+    );
+  }
+
+  /** GET evenement by ID from the server */
+  getEvenementById(id: number): Observable<Evenement> {
+    const url = `${environment.apiUrl}${this.evenementsUrl}/${id}`;
+    return this.http.get<Evenement>(url);
+  }
+
+  /** POST: add a new evenement to the server*/
+  addEvenement(
+    titre: string,
+    description: string,
+    dateEvent: string | null,
+    timeEvent: string | null,
+    lieu: string
+  ): Observable<EvenementUpDTO> {
+    let evenementUpDTO: EvenementUpDTO = {
+      titre: titre,
+      description: description,
+      dateEvent: dateEvent,
+      timeEvent: timeEvent,
+      lieu: lieu,
+    };
+    return this.http.post<EvenementUpDTO>(
+      `${environment.apiUrl}${this.evenementsUrl}`,
+      evenementUpDTO,
+      this.httpOptions
+    );
+  }
+
+  /** DELETE: delete de evenement*/
+  deleteEvenement(id: number): Observable<Evenement> {
+    const url = `${environment.apiUrl}${this.evenementsUrl}/${id}`;
+    return this.http.delete<Evenement>(url, this.httpOptions);
+  }
+
+  /** PUT: update evenement on the server */
+  updateEvenement(evenement: Evenement): Observable<Evenement> {
+    const url = `${environment.apiUrl}${this.evenementsUrl}`;
+    return this.http.put<Evenement>(url, evenement, this.httpOptions).pipe(
+      catchError((error) => {
+        console.error('Error updating evenement:', error);
+        throw error;
+      })
+    );
+  }
+}
